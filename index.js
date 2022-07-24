afaaf095f3b07bab86c12968fdc9738f405dbbca
DucSkii <ducvietdao@live.co.uk>
2022-07-24 17:38:50 +0100

    Fixed game

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const generateComputerHand = () => {
   return possiblePlays[Math.floor(Math.random() * 3)]
 }
 
-let userScore = 0
+let playerScore = 0
 let computerScore = 0
 
 const calculateWinner = (playerHand, computerHand) => {
-  switch (playerHand.toLowerCase()) {
+  switch (playerHand.toLowerCase().trim()) {
     case 'rock':
       if (computerHand === 'scissors') {
+        playerScore++
         console.log(
           `Computer picked ${
             computerHand.charAt(0).toUpperCase() + computerHand.slice(1)
           }, you win!`
         )
-        userScore++
       } else if (computerHand === 'paper') {
+        computerScore++
         console.log(
           `Computer picked ${
             computerHand.charAt(0).toUpperCase() + computerHand.slice(1)
           }, you lose.`
         )
-        computerScore++
       } else {
         console.log(
           `Computer picked ${
@@ -34,19 +34,19 @@ const calculateWinner = (playerHand, computerHand) => {
       break
     case 'paper':
       if (computerHand === 'rock') {
+        playerScore++
         console.log(
           `Computer picked ${
             computerHand.charAt(0).toUpperCase() + computerHand.slice(1)
           }, you win!`
         )
-        userScore++
       } else if (computerHand === 'scissors') {
+        computerScore++
         console.log(
           `Computer picked ${
             computerHand.charAt(0).toUpperCase() + computerHand.slice(1)
           }, you lose.`
         )
-        computerScore++
       } else {
         console.log(
           `Computer picked ${
@@ -57,19 +57,19 @@ const calculateWinner = (playerHand, computerHand) => {
       break
     case 'scissors':
       if (computerHand === 'paper') {
+        playerScore++
         console.log(
           `Computer picked ${
             computerHand.charAt(0).toUpperCase() + computerHand.slice(1)
           }, you win!`
         )
-        userScore++
       } else if (computerHand === 'rock') {
+        computerScore++
         console.log(
           `Computer picked ${
             computerHand.charAt(0).toUpperCase() + computerHand.slice(1)
           }, you lose.`
         )
-        computerScore++
       } else {
         console.log(
           `Computer picked ${
@@ -85,23 +85,27 @@ const play = (playerHand) => {
   calculateWinner(playerHand, computerHand)
 }
 
-const playRound = (playerHand) => {
-  if (typeof playerHand !== 'string') {
-    alert('Please enter "rock", "paper" or "scissors".')
-    document.location.reload()
-  } else if (!possiblePlays.includes(playerHand.toLowerCase())) {
-    alert('Please enter "rock", "paper" or "scissors".')
-    document.location.reload()
-  } else {
-    for (let i = 0; i < 5; i++) {
+const gameStart = () => {
+  for (let i = 0; i < 5; i++) {
+    let playerHand = prompt('Please enter "Rock", "Paper" or "Scissors."')
+    if (typeof playerHand !== 'string') {
+      console.log('Please enter "rock", "paper" or "scissors".')
+      i--
+    } else if (!possiblePlays.includes(playerHand.toLowerCase().trim())) {
+      console.log('Please enter "rock", "paper" or "scissors".')
+      i--
+    } else {
       play(playerHand)
     }
-    console.log(`User: ${userScore} | Computer: ${computerScore}`)
+  }
+  console.log(`User Score: ${playerScore} | Computer Score: ${computerScore}`)
+  if (playerScore < computerScore) {
+    console.log('You Lose.')
+  } else if (playerScore > computerScore) {
+    console.log('You Win!')
+  } else {
+    console.log("It's a draw.")
   }
 }
 
-const gameStart = () => {
-  let playerHand = prompt('Please enter "Rock", "Paper" or "Scissors."')
-  return playRound(playerHand)
-}
 gameStart()
